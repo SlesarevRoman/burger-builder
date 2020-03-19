8f79bb267f8aa74d161bb1f7738d4a121c4cba0a
SlesarevRoman <rslesarev@distillery.com>
2020-03-19 21:44:01 +0300

    lesson 158

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -36,7 +36,8 @@ class Auth extends Component {
         },
         valid: false,
         touched: false,
-      }
+      },
+      isSignUp: true,
     }
   }
 
@@ -74,7 +75,14 @@ class Auth extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
+  }
+
+  swithAuthModeHandler = () => {
+    console.log('clicked');
+    this.setState( (prevState) => {
+      return {isSignUp: !prevState.isSignUp};
+    });
   }
 
   render () {
@@ -105,6 +113,12 @@ class Auth extends Component {
           {form}
           <Button btnType="Success">Submit</Button>
         </form>
+          <Button
+            clicked={this.swithAuthModeHandler}
+            btnType="Danger"
+          >
+             switch to {this.state.isSignUp ? 'Sign In' : 'Sign Up'}
+          </Button>
       </div>
     );
   }
@@ -112,7 +126,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password)),
+    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
   };
 };
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,10 +7,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData,
+    idToken: token,
+    userId: userId,
   };
 };
 
@@ -20,8 +22,29 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    }
+
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCXBfMeoE3M1_rfdFcwchH59T7EM5SvnVE';
+
+    if (!isSignUp) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCXBfMeoE3M1_rfdFcwchH59T7EM5SvnVE';
+    }
+
+    axios.post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      })
   };
 };
